9afe555996c3133e5ddf1e8c103afb3fb6eb4e8e
Jacobo <jacobo@fixel.co.jp>
2020-09-24 18:07:14 +0900

    init on react and added sample component

---
+++ storybook-react/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MyCoolButton } from "./components/sample-component/MyCoolButton";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyCoolButton type="basic" size="giant" label="My Cool Button" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
